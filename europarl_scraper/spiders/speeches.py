c700e64facc1918eab28df5818c4566476ec5afd
kjam <kjarmul@gmail.com>
2016-10-02 09:07:01 +0200

    remove garbage from copying speaker scraper

--- europarl_scraper/spiders/speeches.py
+++ europarl_scraper/spiders/speeches.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors import LinkExtractor
-from europarl_scraper.items import EuroparlMember, EuroparlText
+from europarl_scraper.items import EuroparlText
 import re
 import requests
 
@@ -21,7 +21,7 @@ def get_initial_list():
     return resp.json().get('result')
 
 
-class EuroParlSpeakerSpider(CrawlSpider):
+class EuroParlSpeechSpider(CrawlSpider):
     """ crawl spider for european parliament speakers """
     name = "europarl_speaker"
     allowed_domains = ["europarl.europa.eu"]
@@ -35,7 +35,7 @@ class EuroParlSpeakerSpider(CrawlSpider):
                 allow=r'/meps/en/[\d]+/[A-Z_]+_home.html'
             ),
             follow=True,
-            callback='parse_speaker',),
+        ),
         Rule(
             LinkExtractor(
                 allow=[r'/meps/en/[\d]+/seeall.html?type=[A-Z]+',
@@ -43,20 +43,13 @@ class EuroParlSpeakerSpider(CrawlSpider):
             ),
             follow=True,
         ),
-    )
-    more_rules = """Rule(
-            LinkExtractor(
-                allow=''
-            ),
-            follow=True,
-            callback='parse_speeches',),
         Rule(
             LinkExtractor(
                 allow=''
             ),
             follow=True,
-            callback='parse_debate',),
-        """
+            callback='parse_speeches',),
+    )
 
     def remove_returns(self, my_string):
         """ remove returns from strings """
@@ -80,59 +73,9 @@ class EuroParlSpeakerSpider(CrawlSpider):
             return ''
         return item
 
-    def parse_speaker(self, response):
+    def parse_speeches(self, response):
         """ parse a speaker page and extract items """
         self.response = response
-        item = EuroparlMember()
-        item['speaker_url'] = response.url
-        name = self.grab_xpath('//li[@class="mep_name"]/a/text()')
-        first_name, last_name = [], []
-        for token in name:
-            if token.isupper():
-                last_name.append(token)
-            else:
-                first_name.append(token)
-
-        item['first_name'] = ' '.join(first_name).rstrip(' ')
-        item['last_name'] = ' '.join(last_name).rstrip(' ')
-        item['speaker_id'] = re.search(r'\d+', response.url).group()
-        item['nationality'] = self.grab_xpath(
-            '//li[contains(@class, "nationality")]/text()')
-        dob = self.grab_xpath(
-            '//span[@class="more_info"]/text()')[-1]
-        item['dob'] = dob.lstrip('Date of birth: ').split(',')[0]
-        item['curr_pol_group'] = self.grab_xpath(
-            '//li[contains(@class, "group")]/text()')
-        item['curr_pol_group_abbr'] = self.grab_xpath(
-            '//li[contains(@class, "group")]/@class').lstrip('group ')
-        item['email'] = self.grab_xpath(
-            '//a[@class="link_email"]/@href', return_str=True).lstrip('mailto:')
-        item['website'] = self.grab_xpath(
-            '//a[@class="link_website"]/@href', return_str=True)
-        item['facebook'] = self.grab_xpath(
-            '//ul[@class="link_collection_noborder"]/li/a[@class="link_fb"]/@href',
-            return_str=True)
-        item['twitter'] = self.grab_xpath(
-            '//ul[@class="link_collection_noborder"]/li/a[@class="link_twitt"]/@href',
-            return_str=True)
-
-        # TODO: add nums from _activities.html
-        # activity_page = requests.get(response.url.replace('home', 'activities'))
-
-        item['num_reports'] = 0
-        item['num_opinions'] = 0
-        item['num_motions'] = 0
-        item['num_declarations'] = 0
-        item['num_questions'] = 0
-
-        # history_page = requests.get(response.url.replace('home', 'history'))
-        # TODO: add parsing from _history.html
-
-        item['all_pol_groups'] = []
-        item['natl_pol_groups'] = []
-        item['chair_positions'] = []
-
-        split_url = response.url.split('/')[:-1]
-        split_url.append('seeall.html?type=CRE')
-        item['speeches_url'] = '/'.join(split_url)
+        item = EuroparlText()
+        item['text_url'] = response.url
         return item
